1b6dcdd962da50bcc33fdcef1008da681775bf3a
Gonzalo M. Martinez - OVcM <gonzmmartinez@gmail.com>
2025-10-16 14:30:11 -0300

    Cambios

---
+++ 2025/1.3 Femicidios/X-Femicidios_mapa_argentina.R
@@ -0,0 +1,95 @@
+# Limpiar todo
+rm(list = ls())
+
+# Librerias
+library(openxlsx)
+library(tidyverse)
+library(ggforce)
+library(ggplot2)
+library(ggthemes)
+library(devtools)
+library(rgdal)
+library(geogrid)
+library(sf)
+library(tmap)
+library(readxl)
+library(ggpmisc)
+library(googlesheets4)
+library(gpar)
+
+# Fuentes
+library(showtext)
+font_add_google("Source Sans 3", "font_sans")
+font_add_google("Source Serif 4", "font_serif")
+showtext_auto()
+
+# Cargar datos
+Raw <- read_sheet(ss = "https://docs.google.com/spreadsheets/d/1_n2tTaEXNYTv7fGRLLXt65W49wvFmE3eDpxiBZqmMZk/edit?usp=sharing",
+                  sheet = "Tasa_nacional")
+
+Data <- Raw %>%
+  filter(Año == 2024)
+
+# Cargar shape
+Mapa_Argentina <- rnaturalearth::ne_states(country = c("argentina"), returnclass = "sf") %>%
+  mutate(name = ifelse(name == "Ciudad Autónoma de Buenos Aires", "CABA", name))
+
+# Modificar datos
+Data_femicidios <- Data %>%
+  rename(name = "Provincia")
+Mapa_Argentina <- Mapa_Argentina %>%
+  left_join(Data_femicidios, by="name") %>%
+  replace(is.na(.), 0)
+
+# Colores
+Colores <- colorRampPalette(c("white", "#191717"))(2)
+
+# Tabla
+Tabla <- Data_femicidios %>%
+  select(name, Femicidio, Tasa) %>%
+  rename(Provincia = "name", n = "Femicidio") %>%
+  arrange(desc(Tasa)) %>%
+  mutate(Tasa = formatC(round(Tasa,2), big.mark=".", decimal.mark = ",")) %>%
+  top_n(n=10)
+
+# Theme
+my_ttheme <- 
+  gridExtra::ttheme_default(base_colour="black", base_size=8, base_family="font",
+                            colhead = list(fg_params=list(col="white"),
+                                           bg_params=list(fill="#1da1aa")),
+                            core = list(bg_params=list(fill = "#f2f2f2", col = "white")))
+
+# Grafico
+grafico <- ggplot(Mapa_Argentina) +
+  geom_sf(color="black", aes(fill=Tasa)) +
+  geom_sf_text(aes(label=formatC(round(Tasa,2), big.mark=".", decimal.mark=",")),
+               color="black", family="font_sans", size=2, show_guide=FALSE) +
+  annotate(geom="table", x=-45, y=-47, label = list(Tabla), table.theme=my_ttheme) +
+  theme_void() +
+  guides(fill = guide_colorbar(theme = theme(legend.frame = element_rect(colour = "black")))) +
+  scale_fill_gradient2(name="Tasa de femicidios", low="white", high="#1da1aa", midpoint=0.7, limits=c(min(Data$Tasa),max(Data$Tasa)), breaks=seq(0.5,2, by=0.5)) +
+  theme(text=element_text(family="font_sans"),
+        plot.title = element_text(size=20, family="font_sans", face="bold"),
+        plot.subtitle = element_text(size=15, family="font_sans"),
+        plot.margin = margin(t=0,r=0,b=0,l=0),
+        plot.background = element_rect(fill="white", color=NA),
+        legend.position = "left",
+        legend.justification = "center",
+        legend.margin = margin(t=50, r=-20, b=0, l=5),
+        legend.title = element_text(size=10, family="font_sans", margin=margin(t=0,r=10,b=0,l=0), angle=90),
+        legend.title.position = "left",
+        legend.text = element_text(size=5, family="font_sans"),
+        legend.ticks = element_line(color="black")) +
+  guides(color = guide_legend( 
+    override.aes=list(shape = 16)))
+
+# Guardar gr?fico
+filename <- str_sub(basename(rstudioapi::getSourceEditorContext()$path), 1,
+                    str_length(unlist(basename(rstudioapi::getSourceEditorContext()$path)))-2)
+
+ggsave(filename = paste0(filename, ".png"),
+       path = paste0(dirname(rstudioapi::getActiveDocumentContext()$path),"/Graficos/PNG/"),
+       plot=grafico, dpi=100, width=8, height=8)
+ggsave(filename = paste0(filename, ".pdf"),
+       path=paste0(dirname(rstudioapi::getActiveDocumentContext()$path),"/Graficos/PDF/"),
+       plot=grafico, dpi=72, width=8, height=8)
---
+++ 2025/35-Abusos_SNIC.R
@@ -0,0 +1,83 @@
+# Limpiar todo
+rm(list = ls())
+
+# Funciones
+`%ni%` <- Negate(`%in%`)
+
+# Librer?as
+library(ggplot2)
+library(dplyr)
+library(stringr)
+library(scales)
+library(googlesheets4)
+
+# Fuentes
+library(showtext)
+font_add_google("Source Sans 3", "font_sans")
+font_add_google("Source Serif 4", "font_serif")
+showtext_auto()
+
+# Crear datos
+Data <- data.frame(matrix(c(2014, 318, 503, 821,
+                            2015, 367, 558, 925,
+                            2016, 250, 716, 966,
+                            2017, 290, 870, 1160,
+                            2018, 272, 840, 1112,
+                            2019, 446, 1196, 1642,
+                            2020, 328, 1113, 1441,
+                            2021, 336, 1297, 1633,
+                            2022, 365, 1409, 1774,
+                            2023, 356, 1443, 1799,
+                            2024, 298, 1315, 1613),
+                          nrow=11, byrow=TRUE)) %>%
+  select(-X4) %>%
+  rename(Año = X1) %>%
+  pivot_longer(cols = c(X2, X3), names_to = "Tipo", values_to = "Cantidad") %>%
+  mutate(Tipo = ifelse(Tipo == "X2", "Violaciones", "Otros delitos contra la integridad sexual"))
+
+# Definir colores
+Colores <- c("Violaciones" = "#6e3169",
+             "Otros delitos contra la integridad sexual" = "#e54c7c")
+
+# Gráfico
+grafico <- ggplot(Data, aes(x=Año, y=Cantidad, fill=Tipo)) +
+  geom_col(position="stack", width=0.75) +
+  geom_text(aes(label = formatC(Cantidad, big.mark = ".", decimal.mark=",", format="fg")),
+            family="font_sans", position = position_stack(vjust = 0.5), size=5, color="black") +
+  geom_text(data = Data %>% group_by(Año) %>% summarise(Cantidad = sum(Cantidad)), inherit.aes=FALSE,
+            aes(x=Año, y=Cantidad, label = formatC(Cantidad, big.mark = ".", decimal.mark=",", format="fg")),
+            family="font_sans", size=7.5, fontface="bold", color="black", nudge_y=75) +
+  labs(y="Cantidad de casos", x="Año") +
+  scale_fill_manual(name = str_wrap("Tipo de delito", width=20),
+                    values = Colores) +
+  scale_y_continuous(labels = function(x) formatC(x, big.mark = ".", decimal.mark = ",", format="fg"),
+                     limits= c(0, 2000)) +
+  scale_x_continuous(labels = function(x) formatC(x, big.mark = ".", decimal.mark = ",", format="fg"),
+                     expand = c(0.01, 0.01), breaks = 2014:2024) +
+  theme_light() +
+  theme(text=element_text(family="font_sans"),
+        legend.position="top",
+        legend.justification = "right",
+        legend.title = element_text(size=10, family="font_serif"),
+        legend.text = element_text(size=12, family="font_sans"),
+        legend.key.spacing.x = unit(1, "cm"),
+        plot.title = element_blank(),
+        plot.subtitle = element_blank(),
+        plot.caption = element_blank(),
+        panel.grid = element_blank(),
+        panel.grid.major = element_line(color="grey95", linewidth=0.5),
+        axis.text.x = element_text(size=15, margin = margin(t=10,r=0,b=5,l=0)),
+        axis.text.y = element_text(size=15, margin = margin(t=0,r=10,b=0,l=5)),
+        axis.title.x = element_text(size=20),
+        axis.title.y = element_text(size=20))
+
+# Guardar gr?fico
+filename <- str_sub(basename(rstudioapi::getSourceEditorContext()$path), 1,
+                    str_length(unlist(basename(rstudioapi::getSourceEditorContext()$path)))-2)
+
+ggsave(filename = paste0(filename, ".png"),
+       path = paste0(dirname(rstudioapi::getActiveDocumentContext()$path),"/Graficos/PNG/"),
+       plot=grafico, dpi=100, width=12, height=7)
+ggsave(filename = paste0(filename, ".pdf"),
+       path=paste0(dirname(rstudioapi::getActiveDocumentContext()$path),"/Graficos/PDF/"),
+       plot=grafico, dpi=72, width=12, height=7)
