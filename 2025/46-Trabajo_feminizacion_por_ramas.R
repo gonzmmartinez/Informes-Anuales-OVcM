c33ab338deddf4504e286a4665a802cd51f7414b
Gonzalo M. Martinez <gonzmmartinez@gmail.com>
2025-10-24 15:46:07 -0300

    Cambios

--- 2025/46-Trabajo_feminizacion_por_ramas.R
+++ 2025/46-Trabajo_feminizacion_por_ramas.R
@@ -24,7 +24,8 @@ library(ggrepel)
 
 # Fuentes
 library(showtext)
-font_add_google("Barlow", "font")
+font_add_google("Source Sans 3", "font_sans")
+font_add_google("Source Serif 4", "font_serif")
 showtext_auto()
 
 # Leer datos
@@ -80,44 +81,45 @@ Data <- Resultados1 %>%
                             "Actividades de los hogares", Actividad))
 
 # Colores
-Paleta <- c("#5fad56", "#f2c14e", "#f78154", "#4d9078", "#b4436c")
-Paleta2 <- c("#474E93", "#7E5CAD", "#b4436c", "#72BAA9", "#D5E7B5")
+Paleta <- c("#206170", "#5ec5d4", "#a782ec", "#852f8c", "#0f216d", "#2b42a0",
+            "#ff9d27", "#ff621d", "#f93e35", "#d3335e", "#cbc2ce")
 
-Colores <- c("Varones" = "#7E5CAD",
-             "Mujeres" = "#72BAA9")
+# Colores
+Colores <- c("Varones" = "#852f8c",
+             "Mujeres" = "#ff621d")
 
 # Grafico
 grafico <- ggplot(Data, aes(x=Tasa_feminizacion, y=Ingreso_mediano)) +
   annotate(geom="rect", xmin=1, xmax=45, ymin=300000, ymax=1480000, fill="grey", alpha=0.15, color=NA) +
   annotate(geom="rect", xmin=50, xmax=108, ymin=100000, ymax=900000, fill="grey", alpha=0.15, color=NA) +
   geom_point(aes(color=Tasa_feminizacion), size=5) +
-  geom_text_repel(aes(label=str_wrap(Actividad, 25)), size=2.5, family="font", color="black", alpha=0.75,
+  geom_text_repel(aes(label=str_wrap(Actividad, 25)), size=2.5, family="font_sans", color="black", alpha=0.75,
                   point.padding = 10, segment.color = NA) +
   annotate(geom="text", x=56, y=1250000, label="ramas menos feminizadas\nmayores ingresos promedios",
-           family="font", color="black", size=3, hjust=0.5) +
+           family="font_serif", color="black", size=3, hjust=0.5) +
   annotate(geom="text", x=80, y=1000000, label="ramas más feminizadas\nmenores ingresos promedios",
-           family="font", color="black", size=3, hjust=0.5) +
-  scale_color_gradient2(high="#b4436c", low="#474E93", mid="#b4436c", midpoint = 60) +
+           family="font_serif", color="black", size=3, hjust=0.5) +
+  scale_color_gradient2(high="#d3335e", mid="#938ae4", low="#5ec5d4", midpoint=50) +
   scale_y_continuous(labels = function(z) paste0("$", formatC(z, big.mark = ".", decimal.mark = ",", format="d")),
                      limits = c(0, 1500000), breaks=seq(0,1500000,by=250000), expand = c(0, 0)) +
   scale_x_continuous(labels = function(z) paste0(z, "%"), expand = c(0, 0), limits=c(0,110)) +
   labs(x="Tasa de feminización", y="Ingreso promedio del sector") +
-  theme_linedraw() +
-  theme(text=element_text(family="font"),
+  theme_light() +
+  theme(text=element_text(family="font_sans"),
         legend.position="none",
         legend.justification = "right",
-        legend.title = element_text(size=10, family="font"),
-        legend.text = element_text(size=12, family="font"),
+        legend.title = element_text(size=10, family="font_serif"),
+        legend.text = element_text(size=12, family="font_sans"),
         legend.key.spacing.x = unit(0.5, "cm"),
-        plot.title = element_text(size=20, family="font", face="bold"),
-        plot.subtitle = element_text(size=15, family="font"),
-        plot.caption = element_text(size=12, family="font", face="italic"),
+        plot.title = element_blank(),
+        plot.subtitle = element_blank(),
+        plot.caption = element_text(size=12, family="font_sans", face="italic"),
         panel.grid = element_blank(),
-        panel.grid.major.y = element_line(color="lightgrey"),
+        panel.grid.major = element_line(color="grey95"),
         axis.text.x = element_text(size=12, margin = margin(t=10,r=0,b=5,l=0)),
         axis.text.y = element_text(size=12, margin = margin(t=0,r=10,b=0,l=5)),
-        axis.title.x = element_text(size=15, family="font"),
-        axis.title.y = element_text(size=15, family="font"),
+        axis.title.x = element_text(size=15, family="font_sans"),
+        axis.title.y = element_text(size=15, family="font_sans"),
         plot.margin = unit(c(0.5,0.5,0.5,0.5), "cm"))
 
 # Guardar gráfico
@@ -127,5 +129,6 @@ filename <- str_sub(basename(rstudioapi::getSourceEditorContext()$path), 1,
 ggsave(filename = paste0(filename, ".png"),
        path = paste0(dirname(rstudioapi::getActiveDocumentContext()$path),"/Graficos/PNG/"),
        plot=grafico, dpi=100, width=10, height=6)
-ggsave(filename = paste0(filename, ".pdf"), path=paste0(dirname(rstudioapi::getActiveDocumentContext()$path),"/Graficos/PDF/"),
+ggsave(filename = paste0(filename, ".pdf"),
+       path=paste0(dirname(rstudioapi::getActiveDocumentContext()$path),"/Graficos/PDF/"),
        plot=grafico, dpi=72, width=10, height=6)
