353804889b5554eda8fea657767c835891d47ebf
Gonzalo M. Martinez <gonzmmartinez@gmail.com>
2026-09-21 15:33:54 -0300

    Cambios

--- 2026/01-Requerimientos_911_total.R
+++ 2026/01-Requerimientos_911_total.R
@@ -14,11 +14,18 @@ library(ggtext)
 library(googlesheets4)
 
 # Fuentes
+library(sysfonts)
 library(showtext)
-font_add_google("Source Sans 3", "font_sans")
-font_add_google("Source Serif 4", "font_serif")
+
+dir <- paste0(dirname(rstudioapi::getActiveDocumentContext()$path), "/Fonts/")
+
+font_add("font_title",    file.path(dir, "CreatoDisplay-ExtraBold.otf"))
+font_add("font_subtitle", file.path(dir, "CreatoDisplay-Regular.otf"))
+font_add("font_body",     file.path(dir, "RobotoSlab-Regular.ttf"))
+
 showtext_auto()
 
+# Años
 Año_1 <- 2026
 Año_2 <- 2025
 
@@ -47,7 +54,7 @@ Data1 <- Raw %>%
   mutate(ymid = ymax - (ymax - ymin)/2) %>%
   ungroup() %>%
   mutate(Leyenda = ifelse(Porcentaje >= 10, as.character(Tipo),
-                          paste0(Tipo, " (", formatC(round(Porcentaje,1), big.mark=".", decimal.mark = ","), "%)")))
+                          paste0(Tipo, " (", formatC(Porcentaje, format="f", digits=2, big.mark=".", decimal.mark = ","), "%)")))
 
 Data2 <- Raw %>%
   filter(Año == Año_2, Tipo %nin% c("Abuso sexual", "Abuso sexual (tentativa)")) %>%
@@ -70,14 +77,14 @@ Data2 <- Raw %>%
   ungroup()
 
 # Colores
-Paleta2 <- c("#206170", "#5ec5d4", "#a782ec", "#852f8c", "#0f216d", "#2b42a0",
-             "#ff9d27", "#ff621d", "#f93e35", "#d3335e", "#cbc2ce")
+Paleta2 <- c("#1e7b34", "#119ca0", "#b8d6ac", "#6963aa",
+             "#7c428a", "#4c2158", "#c72a29", "#ec6230", "#cbc2ce")
 
 # Definir colores
-Colores <- c("Violencia de género" = "#f93e35",
-             "Violencia de género histórica" = "#ff621d",
-             "Violencia familiar en curso" = "#a782ec",
-             "Violencia familiar histórica" = "#852f8c")
+Colores <- c("Violencia de género" = "#c72a29",
+             "Violencia de género histórica" = "#ec6230",
+             "Violencia familiar en curso" = "#6963aa",
+             "Violencia familiar histórica" = "#7c428a")
 
 # Total
 Total1 <- paste0( "<span style='font-size:15pt'>Total</span><br>",
@@ -93,10 +100,10 @@ grafico1 <- ggplot(Data1, aes(ymax=ymax, ymin=ymin, xmax=4, xmin=3, fill=Tipo)) 
   geom_rect() +
   geom_textbox(x = 1.5, y = 0, label = Total1, hjust = 0.5,
                halign = 0.5, fill = NA, size=8, box.color=NA,
-               family = "font_sans", lineheight = 0.75) +
+               family = "font_title", lineheight = 0.75) +
   geom_richtext(aes(x = 3.5, y=ymid, label=Label), size=3,
                 color = "white", hjust=0.5, lineheight=1,
-                label.color = NA, family="font_sans",
+                label.color = NA, family="font_body",
                 show.legend=FALSE, fill=NA) +
   coord_polar(theta="y") +
   xlim(c(1.5, 4)) +
@@ -106,12 +113,12 @@ grafico1 <- ggplot(Data1, aes(ymax=ymax, ymin=ymin, xmax=4, xmin=3, fill=Tipo)) 
                     labels=str_wrap(Data1$Leyenda, 25)) +
   labs(title = as.character(Año_1),
        subtitle = "enero-junio") +
-  theme(text=element_text(family="font_sans"),
+  theme(text=element_text(family="font_body"),
         legend.position = "right",
-        plot.title = element_text(family="font_serif", size=25, face="bold", hjust=0.5),
-        plot.subtitle = element_text(family="font_serif", size=10, face="italic", hjust=0.5),
-        legend.title = element_text(size=10, family="font_serif"),
-        legend.text = element_text(size=10, family="font_sans"),
+        plot.title = element_text(family="font_title", size=25, face="bold", hjust=0.5),
+        plot.subtitle = element_text(family="font_subtitle", size=10, face="italic", hjust=0.5),
+        legend.title = element_text(size=10, family="font_subtitle"),
+        legend.text = element_text(size=10, family="font_body"),
         legend.key.spacing.y = unit(0.25, "cm"),
         plot.background = element_rect(fill = "white", colour = NA))
 
@@ -120,10 +127,10 @@ grafico2 <- ggplot(Data2, aes(ymax=ymax, ymin=ymin, xmax=4, xmin=3, fill=Tipo)) 
   geom_rect() +
   geom_textbox(x = 1.5, y = 0, label = Total2, hjust = 0.5,
                halign = 0.5, fill = NA, size=6, box.color=NA,
-               family = "font_sans", lineheight = 1) +
+               family = "font_title", lineheight = 1) +
   geom_richtext(aes(x = 3.5, y=ymid, label=Label), size=3,
                 color = "white", hjust=0.5, lineheight=1,
-                label.color = NA, family="font_sans",
+                label.color = NA, family="font_body",
                 show.legend=FALSE, fill=NA) +
   coord_polar(theta="y") +
   xlim(c(1.5, 4)) +
@@ -131,10 +138,10 @@ grafico2 <- ggplot(Data2, aes(ymax=ymax, ymin=ymin, xmax=4, xmin=3, fill=Tipo)) 
   scale_fill_manual(values = Colores) +
   labs(title = as.character(Año_2),
        subtitle="enero-diciembre") +
-  theme(text=element_text(family="font_sans"),
+  theme(text=element_text(family="font_body"),
         legend.position = "none",
-        plot.title = element_text(family="font_serif", size=25, face="bold", hjust=0.5),
-        plot.subtitle = element_text(family="font_serif", size=10, face="italic", hjust=0.5),
+        plot.title = element_text(family="font_title", size=25, face="bold", hjust=0.5),
+        plot.subtitle = element_text(family="font_subtitle", size=10, face="italic", hjust=0.5),
         legend.title = element_blank(),
         legend.text = element_text(size=15),
         legend.box.margin=margin(5,5,5,5))
--- 2026/02-Requerimientos_911_evolucion.R
+++ 2026/02-Requerimientos_911_evolucion.R
@@ -2,21 +2,30 @@
 rm(list = ls())
 
 # Funciones
-`%ni%` <- Negate(`%in%`)
+`%ni%` <- function(x, table) !(x %in% table)
 
-# Librer?as
+# Librerías
 library(ggplot2)
 library(dplyr)
 library(stringr)
-library(scales)
+library(cowplot)
+library(magick)
+library(ggtext)
 library(googlesheets4)
 
 # Fuentes
+library(sysfonts)
 library(showtext)
-font_add_google("Source Sans 3", "font_sans")
-font_add_google("Source Serif 4", "font_serif")
+
+dir <- paste0(dirname(rstudioapi::getActiveDocumentContext()$path), "/Fonts/")
+
+font_add("font_title",    file.path(dir, "CreatoDisplay-ExtraBold.otf"))
+font_add("font_subtitle", file.path(dir, "CreatoDisplay-Regular.otf"))
+font_add("font_body",     file.path(dir, "RobotoSlab-Regular.ttf"))
+
 showtext_auto()
 
+# Años
 Año_1 <- 2026
 Año_2 <- 2025
 
@@ -51,8 +60,8 @@ Totales <- Data %>%
   summarise(Cantidad = sum(Cantidad))
 
 # Colores
-Paleta <- c("#206170", "#5ec5d4", "#a782ec", "#852f8c", "#0f216d", "#2b42a0",
-            "#ff9d27", "#ff621d", "#f93e35", "#d3335e", "#cbc2ce")
+Paleta <- c("#1e7b34", "#119ca0", "#b8d6ac", "#6963aa",
+             "#7c428a", "#4c2158", "#c72a29", "#ec6230", "#cbc2ce")
 
 # Posiciones
 pos_anios <- c(seq(1.5, 11.5, by = 2), 13)
@@ -62,7 +71,7 @@ pos_lineas <- c(0.25, seq(2.5, 12.5, by = 2), 13.75)
 grafico <- ggplot(Data, aes(x=Semestre_año, y=Cantidad, group=1)) +
   geom_col(aes(fill=Cantidad)) +
   
-  geom_text(aes(label=Label), family="font_sans", size=5,
+  geom_text(aes(label=Label), family="font_body", size=5,
             fontface="bold", color="white", nudge_y=-7500, hjust=0.5) +
   
   # Totales anuales
@@ -78,13 +87,13 @@ grafico <- ggplot(Data, aes(x=Semestre_año, y=Cantidad, group=1)) +
       y=147500,
       label=formatC(Cantidad, big.mark=".", decimal.mark=",", format="fg")
     ),
-    size=5, color="white", family="font_sans", fontface="bold"
+    size=5, color="white", family="font_body", fontface="bold"
   ) +
   
   geom_text(
     data=Totales,
     aes(x=pos_anios, y=154000, label=Año),
-    size=4, color="white", family="font_serif"
+    size=4, color="white", family="font_title"
   ) +
   
   # Años debajo del gráfico
@@ -93,7 +102,7 @@ grafico <- ggplot(Data, aes(x=Semestre_año, y=Cantidad, group=1)) +
     y=-25000,
     x=pos_anios,
     label=2020:2026,
-    size=8, color="black", family="font_sans"
+    size=8, color="black", family="font_body"
   ) +
   
   # Líneas divisorias
@@ -126,16 +135,16 @@ grafico <- ggplot(Data, aes(x=Semestre_año, y=Cantidad, group=1)) +
   ) +
   
   scale_fill_gradient2(
-    high="#2b42a0",
-    low="#95a1d0",
-    mid="#2b42a0",
+    high="#4c2158",
+    low="#C289D2",
+    mid="#833A98",
     midpoint=mean(Data$Cantidad, na.rm=TRUE)
   ) +
   
   scale_color_gradient2(
-    high="#2b42a0",
-    low="#95a1d0",
-    mid="#2b42a0",
+    high="#4c2158",
+    low="#C289D2",
+    mid="#833A98",
     midpoint=mean(Totales$Cantidad, na.rm=TRUE)
   ) +
   
@@ -151,14 +160,14 @@ grafico <- ggplot(Data, aes(x=Semestre_año, y=Cantidad, group=1)) +
   ) +
   
   theme(
-    text=element_text(family="font"),
+    text=element_text(family="font_body"),
     legend.position="none",
     legend.title=element_blank(),
-    legend.text=element_text(size=12, family="font_sans"),
-    plot.title=element_text(size=20, family="font_sans", face="bold"),
-    plot.subtitle=element_text(size=15, family="font_sans"),
+    legend.text=element_text(size=12, family="font_body"),
+    plot.title=element_text(size=20, family="font_title", face="bold"),
+    plot.subtitle=element_text(size=15, family="font_subtitle"),
     plot.caption=element_text(
-      size=10, family="font_sans", face="italic",
+      size=10, family="font_body", face="italic",
       margin=margin(t=10)
     ),
     axis.text.x=element_text(
